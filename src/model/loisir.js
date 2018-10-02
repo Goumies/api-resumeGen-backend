e246088df7fab24010bb85f20d15bc4a9a73fe4f
Romy <contact@romyalula.com>
2018-10-02 10:54:01 +0200

    Reset repo for new folders backend and frontend

--- src/model/loisir.js
+++
@@ -1,8 +0,0 @@
-import mongoose from'mongoose';
-
-const Schema = mongoose.Schema;
-let NewLoisir = new Schema({
-    intitule: {type: String, required: false}
-});
-
-export default mongoose.model('Loisir', NewLoisir);
